fb51d09f04f9acfb712664d1713374bee029cf50
Dishant Padalia <64226014+dishant26@users.noreply.github.com>
2022-07-10 14:19:55 +0530

    Update convert.py

--- convert.py
+++ convert.py
@@ -12,8 +12,8 @@ import numpy as np
 import cv2 as cv
 import subprocess
 from preprocess import deform
-
-
+import shutil
+from pathlib import Path
 
 
 def generator(text_dir,out_dir):
@@ -30,6 +30,7 @@ def generator(text_dir,out_dir):
     imgs_out_dir = os.path.join(out_dir,'images')
     words_dict = dict()
 
+    counter = 1
     for text in tqdm(text_files):
         f = open(text,'r')
         words=f.readline().strip().split(' ')
@@ -37,19 +38,24 @@ def generator(text_dir,out_dir):
             new_img = deform()
             FONT = random.choice(new_img.all_fonts())
             print(f'Font : {FONT}')
-            FONT_ADD = os.path.dirname(new_img.fonts[0])
-            print(f'font add : {os.path.join(FONT_ADD,FONT)}')
-            subprocess.run(f'cp {FONT_ADD} .')
+            FONTS_ADD = os.path.dirname(new_img.fonts[0])
+            print(f'font add : {os.path.join(FONTS_ADD,FONT)}')
+            # subprocess.run(f'cp {FONT_ADD} .')
+            font_add = os.path.join(FONTS_ADD, FONT)
+            font_dest = Path(FONT)
+            shutil.copy(font_add, font_dest)
             font = ImageFont.truetype(FONT,50)
             img = Image.new('RGBA',(500,100),(255,255,255))
             draw = ImageDraw.Draw(img)
             draw.text((0,10),word,fill=(0,0,0),font = font)
             img = np.array(img)
             img = new_img.add_deformity(img)
-            img_loc = os.path.join(imgs_out_dir, f'{word}.png')
+            img_loc = os.path.join(imgs_out_dir, f'img_{counter}.png')
             words_dict[os.path.basename(img_loc)] = word
             cv.imwrite(img_loc,img)
-            subprocess.run(f'rm {FONT}')
+            counter += 1
+            # subprocess.run(f'rm {FONT}')
+            os.remove(FONT)
         f.close()
     out_file = json.dumps(words_dict,ensure_ascii=False)
     out_file_path = os.path.join(out_dir,'labels.json')
@@ -66,8 +72,8 @@ def dir(path):
 
 def args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('text_dir',help='directory containing the text files.',type=dir)
-    parser.add_argument('out_dir',help = 'output dir of the deformed images',type=dir)
+    parser.add_argument('--text_dir',help='directory containing the text files.',type=dir)
+    parser.add_argument('--out_dir',help = 'output dir of the deformed images',type=dir)
     args = parser.parse_args()
     print(f'Text directory : {args.text_dir}, Output directory : {args.out_dir}')
 
